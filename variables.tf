cfedac1dec4388e53c69b3db4b6ac3f4295aab0f
Barry4ever <fran.marquez@gmail.com>
2024-01-23 12:09:23 +0100

    primer stage

--- variables.tf
+++ variables.tf
@@ -1,17 +1,18 @@
 #Variables conexión hyperv
 variable "connect_host" {
     type = string  
-    default = "midsrvhv4"
+    default = "midsrvhv2"
+    #default = "midsrvhv4"
   
 }
 
 variable "connect_username" {
     type = string  
-    default = "midmscs/administrador"   
-  
+    #default = "midmscs/administrador"   
+    default = "darapharma/administrador"
 }
 
 variable "connect_password" {
    type = string
-   default = "Lortnoc2021." 
+   default = "!1OXfj@eEU%x92S6zsbh" 
 }
