097a0b7fc450f7a719ac105190536ac75c20c663
Fran Marquez <fran.marquez@gmail.com>
2024-01-30 09:09:12 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 #Variables conexión hyperv
 variable "connect_host" {
     type = string  
-    default = "midsrvhv2"
-    #default = "midsrvhv4"
+    default = "server1"
+    #default = "server2"
   
 }
 
 variable "connect_username" {
     type = string  
-    #default = "midmscs/administrador"   
-    default = "darapharma/administrador"
+    #default = "************"   
+    default = "****************"
 }
 
 variable "connect_password" {
    type = string
-   default = "!1OXfj@eEU%x92S6zsbh" 
+   default = "********************" 
 }
